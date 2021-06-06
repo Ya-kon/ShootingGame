f328524ee767475a7b470c310d134f2e93012a1e
Ya-kon <seiki.tsubasa@gmail.com>
2021-06-06 16:12:22 +0900

    弾を発射出来るように実装

---
+++ Assets/00_WorkSpace/Bullet/Scripts/BulletMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    #region private variables
+    private bool isDead = false;
+    #endregion // private variables
+
+
+    #region public variables
+    public float speed; // 移動量
+    public float lifeSpan; //生存時間
+    #endregion //public variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeSpan = lifeSpan - Time.deltaTime;
+        if (lifeSpan < 0)
+        {
+            isDead = true;
+        }
+
+        if (!isDead)
+        {
+            this.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        else
+        {
+            Destroy(this.gameObject); //とりあえず破壊
+        }
+    }
+}
---
+++ Assets/00_WorkSpace/Player/Scripts/PlayerAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 攻撃を管理
+*/
+public class PlayerAttack : MonoBehaviour
+{
+    #region constant variables
+    private const string BULLET_NAME = "Bullet";
+    private const string ATTACK_BTN = "Fire1";
+    #endregion //constant variables
+
+    #region private variables
+    private GameObject Bullet = null;
+    private float shotspersecond;  //内部で確認する用
+    #endregion //private variables
+
+    #region public variables
+    public float shotsPerSecond;
+    #endregion //public variables
+
+    /*
+     * 初期化処理 
+    */
+    void Start()
+    {
+        Bullet = (GameObject)Resources.Load(BULLET_NAME); //とりあえずResouce.Loadを使う
+        shotspersecond = shotsPerSecond;
+    }
+
+    /*
+     * 更新処理
+    */
+    void Update()
+    {
+        if(Input.GetButton(ATTACK_BTN))
+        {
+            shotspersecond -= Time.deltaTime;
+
+            if(shotspersecond < 0)
+            {
+                //弾を発射
+                Instantiate(Bullet, this.transform.position, Quaternion.identity);
+                shotspersecond = shotsPerSecond;
+            }
+        }
+    }
+}
