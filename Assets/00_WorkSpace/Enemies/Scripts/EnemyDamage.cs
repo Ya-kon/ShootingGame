97189da33bcd8515a8993e6fd8e5f1fa977565ed
Ya-kon <seiki.tsubasa@gmail.com>
2021-06-06 20:58:08 +0900

    エネミーを追加

---
+++ Assets/00_WorkSpace/Enemies/Scripts/EnemyDamage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    #region public variables
+    public int health;
+    public int damage;
+    #endregion //public variables
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     * トリガーイベント
+    */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet") Damage();
+    }
+
+    /*
+     * ダメージ処理
+    */
+    void Damage()
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
