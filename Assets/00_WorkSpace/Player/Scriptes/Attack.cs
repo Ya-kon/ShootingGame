442be44d4bbbe17ab6b64f9bcf6b35417f1a3bc4
Ya-kon <seiki.tsubasa@gmail.com>
2021-06-06 13:27:02 +0900

    プロジェクト追加 プレイヤーの移動処理を追加

---
+++ Assets/00_WorkSpace/Player/Scriptes/Attack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 攻撃を管理
+*/
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/00_WorkSpace/Player/Scriptes/Move.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 移動を管理
+*/
+public class Move : MonoBehaviour
+{
+    #region constant variables
+    private const float ZERO_INPUT_VAL = 0.0f;
+    private const string XINPUT_NAME = "Horizontal";
+    private const string YINPUT_NAME = "Vertical";
+    #endregion  //constant variables
+
+    # region public variables
+    public float MIN_INPUT_VAL;　//入力除外値
+    public float speedMultiply; //スピードに掛ける値
+    #endregion  //public variables
+
+    #region  private variables
+    private bool isConnectedJoyStick = false; //ゲームパッドが接続されているかどうか
+    private Vector2 speed;　//移動量
+    #endregion //private variables
+
+    /*
+     * 初期化処理 
+    */
+    void Start()
+    {
+
+    }
+
+    /*
+     * 更新処理
+    */
+    void Update()
+    {
+        //ジョイスティックが接続されているかどうかを判断
+        var JoyStickNames = Input.GetJoystickNames();
+        if (JoyStickNames[0] == "") isConnectedJoyStick = false;
+        else isConnectedJoyStick = true;
+
+        //------------------------------
+        // ゲームパッド用入力
+        if (isConnectedJoyStick)
+        {
+            speed.x = Input.GetAxis(XINPUT_NAME);
+            speed.y = Input.GetAxis(YINPUT_NAME);
+
+            //スティック入力値が最低入力値より小さいなら無視
+            if (speed.x < MIN_INPUT_VAL && speed.x > -MIN_INPUT_VAL && speed.y < MIN_INPUT_VAL && speed.y > -MIN_INPUT_VAL)
+            {
+                speed.x = ZERO_INPUT_VAL;
+                speed.y = ZERO_INPUT_VAL;
+            }
+
+        }
+        //------------------------------
+        // キーボード用入力
+        else if (!isConnectedJoyStick)
+        {
+            speed.x = Input.GetAxisRaw(XINPUT_NAME);
+            speed.y = Input.GetAxisRaw(YINPUT_NAME);
+        }
+
+        speed.Normalize(); //正規化
+
+        this.transform.Translate( (speed.x * speedMultiply) * Time.deltaTime, (speed.y * speedMultiply) * Time.deltaTime, 0.0f);　//移動
+    }
+}
